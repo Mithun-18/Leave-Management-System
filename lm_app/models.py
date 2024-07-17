4e3b80b2f122aef898f352118b4458a6013e38c2
Mithun-18 <mithunpoojary180@gmail.com>
2024-07-17 12:52:38 +0530

    Students model created and successfull insert the data to Students model using forms in home.html

--- lm_app/models.py
+++ lm_app/models.py
@@ -1,10 +1,20 @@
 from django.db import models
-
 # Create your models here.
 class User(models.Model):
-    name =models.CharField(max_length=10)
+    user_id =models.CharField(max_length=10)
+    name =models.CharField(max_length=24)
     password =models.TextField(max_length=128)
     
     def __str__(self) -> str:
         return self.name
         
+class Students(models.Model):
+    user_id =models.CharField(max_length=10)
+    date_from=models.DateField()
+    date_to=models.DateField()
+    leave_type=models.CharField(max_length=8)
+    reason=models.TextField(max_length=200)
+    status=models.CharField(max_length=12,default='Pending')
+    
+    def __str__(self) -> str:
+        return self.user_id
--- lm_app/views.py
+++ lm_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import User
 from django.core.exceptions import ObjectDoesNotExist
+from .models import Students
 
 # Create your views here.
 def login(request):
@@ -11,11 +12,11 @@ def login(request):
     
     if(request.method == "POST"):
         try:
-            name = request.POST['userid']
+            user_id = request.POST['userid']
             pwd = request.POST['password']
-            user = User.objects.get(name=name, password=pwd)
+            user = User.objects.get(user_id=user_id, password=pwd)
             if user:
-                request.session['user_session'] = {'userid':user.name}
+                request.session['user_session'] = {'userid':user.user_id}
                 return redirect('/')
         except Exception as ex:
             return render(request,'login.html', {"data": {"error": "Invalid credentials!"}})
@@ -23,7 +24,19 @@ def login(request):
         return render(request,'login.html')
     
 def home(request):
+    if(request.method=='POST'):
+        try:
+            date_from=request.POST['date_from']
+            date_to=request.POST['date_to']
+            user_id = request.session['user_session']['userid']
+            leave_type = request.POST['leave_type']
+            reason = request.POST['reason'] 
+            Students.objects.create(user_id=user_id,date_from=date_from,date_to=date_to,leave_type=leave_type,reason=reason)     
+        except Exception as ex:
+            print(ex,user_id)
+            
     if 'user_session' in request.session:
         return render(request,'home.html')
     else:
         return redirect('/login')
+    
