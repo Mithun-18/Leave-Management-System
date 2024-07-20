730fa4440280eb3a240ffa801919e21a9cef8a27
Mithun-18 <mithunpoojary180@gmail.com>
2024-07-20 17:17:47 +0530

    leave information is started the calculation of total days is pending...

--- leave_management/urls.py
+++ leave_management/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from lm_app.views import login,home
+from lm_app.views import login,home,logout,leave_info
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login),
+    path('logout/', logout),
     path('', home),
+    path('leaves/', leave_info),
 ]
--- lm_app/views.py
+++ lm_app/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from .models import User
 from django.core.exceptions import ObjectDoesNotExist
 from .models import Students
+from django.contrib import messages
+import datetime
 
 # Create your views here.
 def login(request):
@@ -22,21 +24,49 @@ def login(request):
             return render(request,'login.html', {"data": {"error": "Invalid credentials!"}})
     else:
         return render(request,'login.html')
-    
-def home(request):
-    if(request.method=='POST'):
-        try:
-            date_from=request.POST['date_from']
-            date_to=request.POST['date_to']
-            user_id = request.session['user_session']['userid']
-            leave_type = request.POST['leave_type']
-            reason = request.POST['reason'] 
-            Students.objects.create(user_id=user_id,date_from=date_from,date_to=date_to,leave_type=leave_type,reason=reason)     
-        except Exception as ex:
-            print(ex,user_id)
-            
+
+def logout(request):
     if 'user_session' in request.session:
-        return render(request,'home.html')
+        request.session.pop('user_session')
+        return render(request,'logout.html')
     else:
         return redirect('/login')
     
+def home(request):
+    if 'user_session' in request.session:        
+        if(request.method=='POST'):
+            try:
+                date_from=request.POST['date_from']
+                date_to=request.POST['date_to']
+                user_id = request.session['user_session']['userid']
+                leave_type = request.POST['leave_type']
+                reason = request.POST['reason'] 
+                Students.objects.create(user_id=user_id,date_from=date_from,date_to=date_to,leave_type=leave_type,reason=reason)
+                messages.add_message(request, messages.SUCCESS, "Leave added successfully!")  
+            except Exception as ex:
+                print(ex,user_id)
+            return redirect('/')
+        else: 
+            total_leaves=Students.objects.filter(user_id=request.session['user_session']['userid'],status='Approved').count()
+            total_leaves_sick=Students.objects.filter(user_id=request.session['user_session']['userid'],status='Approved',leave_type='sick').count()
+            total_leaves_other=Students.objects.filter(user_id=request.session['user_session']['userid'],status='Approved',leave_type='other').count()
+            total_pending_leaves=Students.objects.filter(user_id=request.session['user_session']['userid'],status='Pending').count()
+            total_rejected_leaves=Students.objects.filter(user_id=request.session['user_session']['userid'],status='Rejected').count()
+            
+            data={
+                "total_leaves":total_leaves,
+                "total_leaves_sick":total_leaves_sick,
+                "total_leaves_other":total_leaves_other,
+                "total_pending_leaves":total_pending_leaves,
+                "total_rejected_leaves":total_rejected_leaves,
+                }
+            return render(request,'home.html',{'data':data})
+    
+    else: 
+        return redirect('/login')
+    
+def leave_info(request):
+    status=request.GET['type']
+    user_id=request.session['user_session']['userid']
+    data=Students.objects.filter(user_id=user_id,status=status).values()
+    return render(request,'leave_info.html',{'data':data,'status':status})
